9f1fcd3e279da14d1a4a0e9ab53ede0df748b0bc
wolf_ABF92E8297 <wolf@cyber-dojo.org>
2015-04-25 03:57:14 +0000

    14

--- sandbox/ReorderTest.java
+++ sandbox/ReorderTest.java
@@ -13,13 +13,13 @@ public class ReorderTest {
     }
 
     @Test
-    public void testOrders_One() {
+    public void testOrders_1() {
         List actual = Reorder.orders(Arrays.asList(1));
         assertEquals(Arrays.asList(Arrays.asList(1)), actual);
     }
 
     @Test
-    public void testOrders_two() {
+    public void testOrders_12() {
         List actual = Reorder.orders(Arrays.asList(1,2));
         List<List> expect = Arrays.asList(Arrays.asList(1,2),
             Arrays.asList(2,1));
@@ -27,7 +27,7 @@ public class ReorderTest {
     }
     
     @Test
-    public void testOrders_twosame() {
+    public void testOrders_11() {
         List actual = Reorder.orders(Arrays.asList(1,1));
         List<List> expect = Arrays.asList(Arrays.asList(1,1));
         assertEquals(expect, actual);        
