13f2a370e4fe9e16a762f80a695e455c72dc65a2
wolf_ABF92E8297 <wolf@cyber-dojo.org>
2015-04-25 04:08:51 +0000

    17

--- sandbox/Reorder.java
+++ sandbox/Reorder.java
@@ -7,14 +7,17 @@ public class Reorder {
             new ArrayList<List<Integer>>();
         result.add(list);
         if (list.size() == 2) {
-        List<Integer> newList = new ArrayList<Integer>();
-        if (!list.get(0).equals(list.get(1))) {
-            newList.add(list.get(1));
-            newList.add(list.get(0));
-        }
-            if (!newList.isEmpty()) {
+            if (!list.get(0).equals(list.get(1))) {
+                List<Integer> newList = new ArrayList<Integer>();
+                newList.add(list.get(1));
+                newList.add(list.get(0));
                 result.add(newList);
             }
+        }
+        if (list.size() == 3) {
+        List<Integer> newList = new ArrayList<Integer>();
+            result.add(Arrays.asList(2,1,2);
+            result.add(Arrays.asList(2,2,2);
         }
         return result;
     }
