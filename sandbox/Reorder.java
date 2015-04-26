d8e9ef33939db5740437bb5c9ade4da3c1dcd4c3
wolf_ABF92E8297 <wolf@cyber-dojo.org>
2015-04-26 07:25:49 +0000

    68

--- sandbox/Reorder.java
+++ sandbox/Reorder.java
@@ -9,7 +9,7 @@ public class Reorder {
         if (list.size() >= 1) {
             for (int i=1; i<=list.size(); i++) {
                 int toMix = list.get(list.size()-i);
-                ArrayList<List<Integer>> temp = new ArrayList<list<Integer>>();
+                ArrayList<List<Integer>> temp = new ArrayList<List<Integer>>();
                 for (List<Integer> partList : result)
                     temp.addAll(mixInto(toMix, partList));
                 result = temp;
